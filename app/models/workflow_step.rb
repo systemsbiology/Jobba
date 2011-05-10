bdfd3d1b9c48a33a312d6fc503fe04bd62f5f2fa
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-05-10 12:40:00 -0700

    Fix WorkflowStep for refactoring from TaskStep

--- app/models/workflow_step.rb
+++ app/models/workflow_step.rb
@@ -2,8 +2,7 @@ class WorkflowStep
   include Mongoid::Document
   include ActsAsList::Mongoid 
 
-  field :task_id, :type => Integer
   field :name, :type => String
   field :status, :type => String
-  embedded_in :workflow, :inverse_of => :task_steps
+  embedded_in :workflow, :inverse_of => :workflow_steps
 end
