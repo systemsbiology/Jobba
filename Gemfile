6f47960259c4f881dcbe3ea47363e7b4d16d6087
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-06-24 10:30:03 -0700

    Switch from infinity_test to guard for continuous testing

--- Gemfile
+++ Gemfile
@@ -2,42 +2,47 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.6'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3'
 
-# Use unicorn as the web server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# Bundle the extra gems:
-# gem 'bj'
-# gem 'nokogiri'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'aws-s3', :require => 'aws/s3'
-
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
 group :development, :test do
   gem 'rspec-rails', '~> 2.5.0'
 
-  # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+  # debugger for ruby 1.9
   gem 'ruby-debug19', :require => 'ruby-debug'
+
+  # debugger for ruby 1.8
   # gem 'ruby-debug'
-  gem 'infinity_test'
+  
+  # run specs continuously
+  gem 'guard-rspec'
+
+  # speed up tests with spork
+  gem 'guard-spork'
+
+  # use the RC to get around issues with 0.8.x
+  gem 'spork', '0.9.0.rc8'
+
+  # use FSEvent support in guard for OS X
+  gem 'rb-fsevent'
+
+  # for guard notifications in OS X
+  gem 'growl'
+
+  # rspec formatter with progress bar and instafail
+  gem 'fuubar'
 end
 
+# ruote
 gem "ruote", ">=2.2.0"
 gem "ruote-kit", ">=2.2.0"
+gem 'erwin-ruote-mongodb', :git=>"git://github.com/erwin/ruote-mongodb.git"
+
+# mongoid and friends
 gem "mongoid", "~>2.0.0"
 gem "bson_ext"
-gem 'erwin-ruote-mongodb', :git=>"git://github.com/erwin/ruote-mongodb.git"
 gem 'acts_as_list_mongoid', :git=>"git://github.com/kristianmandrup/acts_as_list_mongoid.git"
 gem 'mongoid_embedded_helper', :git => "git://github.com/kristianmandrup/mongoid_embedded_helper.git"
 gem 'mongoid_adjust', :git => "git://github.com/kristianmandrup/mongoid_adjust.git"
-gem 'bulk_api'
+
+# this gets around gems that are using json_pure and having problems
 gem 'json', '1.5.3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,42 +1,50 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  RSpec.configure do |config|
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  #config.use_transactional_fixtures = true
-  
-  config.before(:each) do
-    puts "cleaning mongodb...." if $DEBUG
-    Mongoid.database.collections.each do |collection|
-      unless collection.name =~ /^system\./
-        collection.remove
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    #config.use_transactional_fixtures = true
+    
+    config.before(:each) do
+      puts "cleaning mongodb...." if $DEBUG
+      Mongoid.database.collections.each do |collection|
+        unless collection.name =~ /^system\./
+          collection.remove
+        end
       end
-    end
 
-    RUOTE_STORAGE.purge_type!("expressions")
-    RUOTE_STORAGE.purge_type!("errors")
-    RUOTE_STORAGE.purge_type!("msgs")
-    RUOTE_STORAGE.purge_type!("variables")
-    puts "finished cleaning mongodb." if $DEBUG
+      RUOTE_STORAGE.purge_type!("expressions")
+      RUOTE_STORAGE.purge_type!("errors")
+      RUOTE_STORAGE.purge_type!("msgs")
+      RUOTE_STORAGE.purge_type!("variables")
+      puts "finished cleaning mongodb." if $DEBUG
+    end
   end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
 end
